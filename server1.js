452d762aaf824fd4bfd5c93da9699733f9c9e31b
jantapabh <jantapa2407@gmail.com>
2020-01-16 19:11:52 +0700

    test homework

--- server1.js
+++ server1.js
@@ -3,20 +3,45 @@
 var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 6969;
+var number = [5, 8, 10, 15, 19];
+let i = 0;
 
 
 net.createServer(function (sock) {
-    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+
+    console.log('Strat connect : ' + sock.remoteAddress + ':' + sock.remotePort);
+
+
     sock.on('data', function (data) {
 
         // console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        
-        sock.write("OK");
+
+        sock.write("OK !!!!!!!"); //่ส่วนตอบกลับไยัง client
+
     });
 
     sock.on('close', function (data) {
-        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+        // console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+
+        if (i < 5) {
+            i = i + 1;
+            if (data == number) {
+                sock.write("BINGO");
+            }
+            else {
+
+                sock.write("WRONG");
+
+            }
+        }
+
     });
+
+    sock.on('error', function (error) {
+        console.log('ERROR Connect : ' + sock.remoteAddress + ' ' + sock.remotePort);
+    });
+
+
 }).listen(PORT, HOST);
 
-console.log('Server listening on ' + HOST + ':' + PORT);
+// console.log('Server listening on ' + HOST + ':' + PORT);
--- servergame.js
+++ servergame.js
@@ -15,7 +15,7 @@ var name = "6035512034"
 net.createServer(function (sock) {
 
     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-    
+
     sock.on('data', function (data) {
         console.log('DATA ' + sock.remoteAddress + ': ' + data);
         sock.write('You said "' + data + '"');
@@ -24,6 +24,7 @@ net.createServer(function (sock) {
     sock.on('close', function (data) {
         console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
     });
+    
 }).listen(PORT, HOST);
 
 console.log('Server listening on ' + HOST + ':' + PORT);
