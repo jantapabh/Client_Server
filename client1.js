e8d96b96de36d1f42fc1fbf5603e3083816fc454
jantapabh <jantapa2407@gmail.com>
2020-01-16 19:41:07 +0700

    test

--- client1.js
+++ client1.js
@@ -4,19 +4,48 @@
 var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 6969;
+let i = 0;
+
 
 var client = new net.Socket();
 client.connect(PORT, HOST, function () {
 
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    client.write('Jan');
+    client.write('6035512034');
 
 });
 
 client.on('data', function (data) {
-    console.log('DATA: ' + data);
-    client.destroy();
+
+    // console.log('DATA: ' + data);
+    // client.destroy();
+
+    if( i < 5 ){
+
+        i = i+ 1;
+
+        if(data == "BINGO")
+        {
+    
+            console.log("Number Correct");
+            client.destroy();
+        }
+        else{
+
+            var answer = Math.floor(Math.random() * 21);
+            word = parseInt(answer);
+            client.write(word+'');
+            // console.log("Send Success");
+
+        }
+    }
+       
 });
+
+client.on('error', function (error) {
+    console.log('ERROR Connect : ' + client.remoteAddress + ' ' + client.remotePort);
+});
+
 
 // Add a 'close' event handler for the client socket
 client.on('close', function () {
--- server1.js
+++ server1.js
@@ -3,37 +3,54 @@
 var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 6969;
-var number = [5, 8, 10, 15, 19];
-let i = 0;
+var number = [5,8,10,15,19];
+var i = 0;
 
 
 net.createServer(function (sock) {
 
+    
     console.log('Strat connect : ' + sock.remoteAddress + ':' + sock.remotePort);
 
+    sock.write("OK");
+//     if( i < 5){
 
+//         i = i + 1;
+
+//     if( data == number)
+//     {
+//         sock.write("BINGO");
+//         console.log(' ',+data);
+//         console.log("BINGO");
+
+//     }
+//     else
+//     {
+
+//         sock.write("WRONG");
+//         console.log(' ',+data);
+
+//     }
+// }
+    
     sock.on('data', function (data) {
 
         // console.log('DATA ' + sock.remoteAddress + ': ' + data);
-
-        sock.write("OK !!!!!!!"); //่ส่วนตอบกลับไยัง client
+        sock.write("OK");
+ 
+        
+        //่ส่วนตอบกลับไยัง client
 
     });
 
     sock.on('close', function (data) {
+
+
         // console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
-
-        if (i < 5) {
-            i = i + 1;
-            if (data == number) {
-                sock.write("BINGO");
-            }
-            else {
-
-                sock.write("WRONG");
-
-            }
-        }
+       
+        console.log("END");
+        // sock.destroy();
+    
 
     });
 
