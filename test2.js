e57537e427a151bffa0d914a09bcb5f2eb7e6135
jantapabh <jantapa2407@gmail.com>
2020-01-17 21:11:38 +0700

    test

--- test2.js
+++ test2.js
@@ -2,6 +2,8 @@ var net = require('net');
 
 var HOST = '127.0.0.1';
 var PORT = 6969;
+
+
 net.createServer(function(sock) {
    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
    sock.on('data', function(data) {
@@ -9,9 +11,12 @@ net.createServer(function(sock) {
        sock.write('You said "' + data + '"');
    });
 
+
    sock.on('close', function(data) {
        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
    });
+
+    
 }).listen(PORT, HOST);
 
 console.log('Server listening on ' + HOST +':'+ PORT);
