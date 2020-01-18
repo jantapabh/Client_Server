adbac57da1192e19f004c16aab80a8dc29f9efed
jantapabh <jantapa2407@gmail.com>
2020-01-18 23:20:22 +0700

    ..

--- test2.js
+++ test2.js
@@ -9,6 +9,8 @@ var PORT = 6969;
 net.createServer(function(sock) {
 
 
+
+    
    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
    sock.on('data', function(data) {
        console.log('DATA ' + sock.remoteAddress + ': ' + data);
@@ -24,7 +26,7 @@ net.createServer(function(sock) {
    });
 
     
-   
+
 }).listen(PORT, HOST);
 
 console.log('Server listening on ' + HOST +':'+ PORT);
